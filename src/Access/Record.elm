5ca381f26b37ad690847452ea40a4454872f11c8
notnew <you@example.com>
2015-08-12 16:37:29 -0500

    add updater functions for records

--- src/Access/Record.elm
+++ src/Access/Record.elm
@@ -11,3 +11,13 @@ unsafeSetter : (oldRec -> b) -> new -> oldRec -> newRec
 unsafeSetter getter =
   Native.Access.makeRecordSetter getter
 
+updater : (rec -> b) -> (b -> b) -> rec -> rec
+updater =
+  unsafeUpdater
+
+unsafeUpdater : (oldRec -> b) -> (b -> new) -> oldRec -> newRec
+unsafeUpdater getter update rec =
+  let old = getter rec
+      new = update old
+      set = unsafeSetter getter
+  in set new rec
