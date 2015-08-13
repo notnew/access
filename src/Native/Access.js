e45434fcff86e2ae8dd67f5ddfd7b7e9e3a3d641
notnew <you@example.com>
2015-08-13 12:45:27 -0500

    cleanup Native javascript

--- src/Native/Access.js
+++ src/Native/Access.js
@@ -1,16 +1,17 @@
 Elm.Native.Access = {};
 Elm.Native.Access.make = function (elm) {
-  elm.Native = elm.Native || {};
-  elm.Native.Utils = elm.Native.Utils || {};
-  _Utils = elm.Native.Utils.values;
-  _Debug = Elm.Debug.make(elm);
 
+  elm.Native = elm.Native || {};
   elm.Native.Access = elm.Native.Access || {};
+
   if (elm.Native.Access.values) {
     return elm.Native.Access.values;
   }
 
-  function makeRecordSetter (getter) {
+  _Utils = Elm.Native.Utils.make(elm);
+  _Debug = Elm.Debug.make(elm);
+
+  function makeRecordSetter (getter, newVal, record) {
     var pattern = /return _.([^;]+);/;
     var source = getter.toString();
     var match = pattern.exec(source);
@@ -27,13 +28,11 @@ Elm.Native.Access.make = function (elm) {
 
     var fieldName = match[1];
 
-    return F2(function (newVal, record) {
-      return _Utils.replace( [[fieldName, newVal]], record);
-    });
+    return _Utils.replace( [[fieldName, newVal]], record);
   };
 
   return elm.Native.Access.values = {
-    makeRecordSetter : makeRecordSetter
+    makeRecordSetter : F3(makeRecordSetter)
   };
 
 };
